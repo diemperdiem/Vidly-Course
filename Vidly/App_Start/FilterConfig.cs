4c9f24048130e178e2c4338ec8310000b58d03e7
Gabriel Medina <38697401+diemperdiem@users.noreply.github.com>
2020-02-21 12:43:54 -0800

    Descripcion Actualizada

--- Vidly/App_Start/FilterConfig.cs
+++ Vidly/App_Start/FilterConfig.cs
@@ -9,7 +9,7 @@ namespace Vidly
         {
             filters.Add(new HandleErrorAttribute()); //para que todos los errores se manejen por la view error.html
             filters.Add(new AuthorizeAttribute()); //Para restringir globalmente el acceso al pag si se quiere dejar entrar como anonimo a alguna pestaña poner [AllowAnonimous]/algo asi
-            filters.Add(new RequireHttpsAttribute()); //supongo que es algo de las apis
+            filters.Add(new RequireHttpsAttribute()); //Adding this attribute would require that any traffic be sent over a secure channel (i.e. https) as opposed to http.
         }
     }
 }
